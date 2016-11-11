7581c27716cd3a08f53567998ac108fca86fad5a
Johannus V <johannus.vogel@gmail.com>
2016-11-11 02:55:50 -0600

    Bot model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161111085439) do
+
+  create_table "bots", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
