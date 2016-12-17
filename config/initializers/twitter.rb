4c49264f405208b811bba8913442d2f9fa899462
Johannus Vogel <johannus.vogel@gmail.com>
2016-12-17 02:17:34 -0600

    Adding auto responder to his holyness

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,7 +1,38 @@
 
-CLIENT = Twitter::REST::Client.new do |config|
-  config.consumer_key = ENV['consumer_key']
-  config.consumer_secret = ENV['consumer_secret']
-  config.access_token = ENV['access_token']
-  config.access_token_secret = ENV['access_secret']
+config = {
+  consumer_key: ENV['consumer_key'],
+  consumer_secret: ENV['consumer_secret'],
+  access_token: ENV['access_token'],
+  access_token_secret: ENV['access_secret'],
+}
+
+CLIENT = Twitter::REST::Client.new(config)
+#
+# STREAMING_CLIENT = Twitter::Streaming::Client.new do |config|
+#   config.consumer_key = ENV['consumer_key']
+#   config.consumer_secret = ENV['consumer_secret']
+#   config.access_token = ENV['access_token']
+#   config.access_token_secret = ENV['access_secret']
+# end
+
+
+Thread.new do
+  loop do
+    begin
+
+      rClient = CLIENT
+      sClient = Twitter::Streaming::Client.new(config)
+      sClient.user do |object|
+        if object.is_a? Twitter::Streaming::Event and object.name==:follow
+          user = object.source
+            rClient.create_direct_message user, "You, #{user.name}, are also holy! Holy #{user.name}!"
+            puts "New follower : #{user.name}"
+        end
+      end
+
+    rescue
+      puts 'error occurred, sleeping for 5 seconds'
+      sleep 5
+    end
+  end
 end
